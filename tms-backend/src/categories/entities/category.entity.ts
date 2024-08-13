83bbdced59e8c2a4238493d2f04eef88e6a9238f
QuanDH <hongquan.contact@gmail.com>
2024-08-13 14:55:27 +0700

    change config tsconfig

--- tms-backend/src/categories/entities/category.entity.ts
+++ tms-backend/src/categories/entities/category.entity.ts
@@ -1,21 +1,29 @@
-import { Activity } from 'src/activities/entities/activity.entity';
-import { User } from 'src/users/entities/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
+import { Activity } from '../../activities/entities/activity.entity';
+import { User } from '../../users/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
 
 @Entity()
 export class Category {
-    @PrimaryGeneratedColumn()
-    id: number;
-    @Column({ type: 'varchar', length: 255 })
-    categoryName: string;
-    @Column()
-    userId: number;
-    @CreateDateColumn()
-    createdAt: Date;
-    @UpdateDateColumn()
-    updatedAt: Date;
-    @ManyToOne(() => User, user => user.categories)
-    user: User;
-    @OneToMany(() => Activity, activity => activity.category)
-    activities: Activity[];
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ type: 'varchar', length: 255 })
+  categoryName: string;
+  @Column()
+  userId: number;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+  @ManyToOne(() => User, (user) => user.categories)
+  user: User;
+  @OneToMany(() => Activity, (activity) => activity.category)
+  activities: Activity[];
 }
--- tms-backend/src/users/dto/create-user.dto.ts
+++ tms-backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+// src/users/dto/create-user.dto.ts
+import { IsString, IsEmail, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(4, 255)
+  userName: string;
+
+  @IsString()
+  @Length(6, 255)
+  passWord: string;
+}
