4110ed450ab922a65211415b631ed3472c025979
QuanDH <hongquan.contact@gmail.com>
2024-08-14 15:50:37 +0700

    add test case category and implement it

--- tms-backend/src/categories/categories.service.spec.ts
+++ tms-backend/src/categories/categories.service.spec.ts
@@ -1,18 +1,130 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Repository } from 'typeorm';
+import { BaseResponse } from '@/common/base-response/base-response.dto';
+import { buildError } from '../common/utils/Utility';
+import { ErrorMessage } from '../common/utils/error-const';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { User } from '../users/entities/user.entity';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let repository: Repository<Category>;
+  let userRepository: Repository<User>;
 
+  // Mock repository
+  const mockCategoryRepository = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+  };
+  const mockUserRepository = () => ({
+    findOne: jest.fn(),
+  });
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        {
+          provide: getRepositoryToken(Category),
+          useValue: mockCategoryRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
+    repository = module.get<Repository<Category>>(getRepositoryToken(Category));
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findCategoriesByUserId', () => {
+    it('should return a list of categories by userId', async () => {
+      const userId = 1;
+      const mockCategories = {
+        data: [
+          { id: 1, name: 'Category1', userId: 1 },
+          { id: 2, name: 'Category2', userId: 1 },
+        ],
+        isSuccess: true,
+        message: 'Categories found successfully',
+      } as BaseResponse;
+
+      mockCategoryRepository.find.mockResolvedValue(mockCategories.data);
+
+      const result = await service.findCategoriesByUserId(userId);
+      expect(result).toEqual(mockCategories);
+      expect(repository.find).toHaveBeenCalledWith({ where: { userId } });
+    });
+
+    it('should return an error response if no categories are found', async () => {
+      const userId = 2;
+      const mockErrorResponse = buildError(
+        ErrorMessage.CATEGORIES_NOT_FOUND,
+      ) as BaseResponse;
+
+      mockCategoryRepository.find.mockResolvedValue([]);
+
+      const result = await service.findCategoriesByUserId(userId);
+      expect(result).toEqual(mockErrorResponse);
+      expect(repository.find).toHaveBeenCalledWith({ where: { userId } });
+    });
+  });
+  describe('create', () => {
+    it('should create and return a new category', async () => {
+      const createCategoryDto: CreateCategoryDto = {
+        name: 'New Category',
+        userId: 1,
+      };
+
+      const user = new User();
+      user.id = createCategoryDto.userId;
+
+      const category = new Category();
+      category.id = 1;
+      category.name = createCategoryDto.name;
+      category.userId = createCategoryDto.userId;
+
+      const expectedResponse: BaseResponse = {
+        data: category,
+        isSuccess: true,
+        message: 'Category created successfully',
+      };
+
+      userRepository.findOne = jest.fn().mockResolvedValue(user);
+      repository.create = jest.fn().mockReturnValue(category);
+      repository.save = jest.fn().mockResolvedValue(category);
+
+      expect(await service.create(createCategoryDto)).toEqual(expectedResponse);
+    });
+
+    it('should return error if user is not found', async () => {
+      const createCategoryDto: CreateCategoryDto = {
+        name: 'New Category',
+        userId: 999, // Assuming this ID does not exist
+      };
+
+      const expectedResponse: BaseResponse = {
+        data: null,
+        isSuccess: false,
+        message: 'User not found',
+      };
+
+      userRepository.findOne = jest.fn().mockResolvedValue(null);
+
+      expect(await service.create(createCategoryDto)).toEqual(expectedResponse);
+    });
+
+    // Add additional tests as needed
   });
 });
--- tms-backend/src/common/utils/error-const.ts
+++ tms-backend/src/common/utils/error-const.ts
@@ -4,4 +4,6 @@ export enum ErrorMessage {
   EMAIL_IS_REQUIRED = 'Email is required',
   USERNAME_IS_REQUIRED = 'Username is required',
   PASSWORD_IS_REQUIRED = 'Password is required',
+  CATEGORIES_NOT_FOUND = 'Categories not found',
+  USER_NOT_FOUND = 'User not found',
 }
