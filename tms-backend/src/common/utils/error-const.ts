7fabf8a7f53a11dd7ea972e4b96029b6e5e3d38d
QuanDH <hongquan.contact@gmail.com>
2024-08-14 12:02:00 +0700

    change dto response

---
+++ tms-backend/src/common/utils/error-const.ts
@@ -0,0 +1,7 @@
+export enum ErrorMessage {
+  LOGIN_FAILED = 'Login failed',
+  EMAIL_ALREADY_EXISTS = 'Email already exists',
+  EMAIL_IS_REQUIRED = 'Email is required',
+  USERNAME_IS_REQUIRED = 'Username is required',
+  PASSWORD_IS_REQUIRED = 'Password is required',
+}
