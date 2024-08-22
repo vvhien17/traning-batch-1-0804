fc0535a7375c2d8726250a93ff16b80339a4a97e
HoaiBaoNguyen <hoaibao2129@gmail.com>
2024-08-22 15:10:55 +0700

    add unit test feature create activity

--- tms-backend/src/activity/activity.service.spec.ts
+++ tms-backend/src/activity/activity.service.spec.ts
@@ -4,9 +4,12 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Activity } from './entities/activity.entity';
 import { Repository } from 'typeorm';
 import { BadRequestException } from '@nestjs/common';
-import { ErrorMessage } from '../common/utils/message-const';
-
+import { ErrorMessage, SuccessMessage } from '../common/utils/message-const';
+import { BaseResponse } from '../common/base-response/base-response.dto';
+import { buildError } from '../common/utils/Utility';
 const currentDate = new Date();
+const endedAt = new Date();
+endedAt.setDate(endedAt.getDate() - 1);
 const mockActivities = [
   {
     id: 1,
@@ -26,7 +29,6 @@ const mockActivities = [
     updatedAt: currentDate,
     startedAt: currentDate,
     endedAt: currentDate,
-    description: 'test desc',
   },
 ];
 
@@ -42,6 +44,8 @@ describe('ActivitiesController', () => {
           useValue: {
             find: jest.fn().mockResolvedValue(mockActivities),
             findOne: jest.fn(),
+            create: jest.fn().mockResolvedValue(mockActivities[0]),
+            save: jest.fn().mockResolvedValue(mockActivities[0])
           },
         },
       ],
@@ -105,4 +109,181 @@ describe('ActivitiesController', () => {
       expect(activity.data).toEqual(mockActivities[0]);
     });
   });
+
+  describe("create activity", () => {
+    it("create a new activity enough require without description", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startedAt: currentDate,
+        endedAt: endedAt,
+        categoryId: 1,
+      }
+      jest.spyOn(activityRepository, 'save').mockResolvedValue(mockActivities as Activity);
+      jest.spyOn(activityRepository, "create").mockReturnValue(mockActivities as Activity)
+      const result: any = await service.create(mockActivities)
+      expect(result.data).toEqual(mockActivities as Activity)
+      expect(result.isSuccess).toBe(true)
+      expect(result.message).toEqual(SuccessMessage.CREATE_DATA_SUCCESS)
+    })
+
+    it("create a new activity enough require with description", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startedAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      jest.spyOn(activityRepository, 'save').mockResolvedValue(mockActivities as Activity);
+      jest.spyOn(activityRepository, "create").mockReturnValue(mockActivities as Activity)
+      const result: any = await service.create(mockActivities)
+      expect(result.data).toEqual(mockActivities as Activity)
+      expect(result.isSuccess).toBe(true)
+      expect(result.message).toEqual(SuccessMessage.CREATE_DATA_SUCCESS)
+    })
+
+    it("Create a new activity missing only startedDate", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      const expectedResponse: BaseResponse = buildError(
+        ErrorMessage.BAD_REQUEST,
+      );
+      const result: any = await service.create(mockActivities)
+      expect(result).toEqual(expectedResponse);
+    })
+
+    it("Create a new activity missing only endedDate", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startAt: currentDate,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      const expectedResponse: BaseResponse = buildError(
+        ErrorMessage.BAD_REQUEST,
+      );
+      const result: any = await service.create(mockActivities)
+      expect(result).toEqual(expectedResponse);
+    })
+
+    it("Create a new activity missing only name activity", async () => {
+      const mockActivities = {
+        id: 1,
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      const expectedResponse: BaseResponse = buildError(
+        ErrorMessage.BAD_REQUEST,
+      );
+      const result: any = await service.create(mockActivities)
+      expect(result).toEqual(expectedResponse);
+    })
+
+    it("Create a new activity with require data and not choosing category", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startedAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+      }
+      jest.spyOn(activityRepository, 'save').mockResolvedValue(mockActivities as Activity);
+      jest.spyOn(activityRepository, "create").mockReturnValue(mockActivities as Activity)
+      const result: any = await service.create(mockActivities)
+      expect(result.data).toEqual(mockActivities as Activity)
+      expect(result.isSuccess).toBe(true)
+      expect(result.message).toEqual(SuccessMessage.CREATE_DATA_SUCCESS)
+    })
+
+    it("Create a new activity with require data and choosing category", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startedAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      jest.spyOn(activityRepository, 'save').mockResolvedValue(mockActivities as Activity);
+      jest.spyOn(activityRepository, "create").mockReturnValue(mockActivities as Activity)
+      const result: any = await service.create(mockActivities)
+      expect(result.data).toEqual(mockActivities as Activity)
+      expect(result.isSuccess).toBe(true)
+      expect(result.message).toEqual(SuccessMessage.CREATE_DATA_SUCCESS)
+    })
+
+    it("Create new activity with exist name enough require data", async () => {
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startedAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      jest.spyOn(activityRepository, 'save').mockResolvedValue(mockActivities as Activity);
+      jest.spyOn(activityRepository, "create").mockReturnValue(mockActivities as Activity)
+      const result: any = await service.create(mockActivities)
+      expect(result.data).toEqual(mockActivities as Activity)
+      expect(result.isSuccess).toBe(true)
+      expect(result.message).toEqual(SuccessMessage.CREATE_DATA_SUCCESS)
+    })
+
+
+    it("Create new activity enough data but endedDate before startedDate", async () => {
+      const endedAt = new Date();
+      endedAt.setDate(endedAt.getDate() + 1);
+      const mockActivities = {
+        id: 1,
+        name: 'Activity 1',
+        userId: 1,
+        createdAt: currentDate,
+        updatedAt: currentDate,
+        startedAt: currentDate,
+        endedAt: endedAt,
+        description: 'test desc',
+        categoryId: 1,
+      }
+      const expectedResponse: BaseResponse = buildError(
+        ErrorMessage.BAD_REQUEST,
+      );
+      const result: any = await service.create(mockActivities)
+      expect(result).toEqual(expectedResponse);
+    })
+
+  })
 });
