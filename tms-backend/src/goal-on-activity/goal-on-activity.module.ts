73a240ec811ac223f306813f9ee9701d067fd179
hienvv17 <hien.vo@xpodc.com>
2024-08-22 15:59:58 +0700

    update entity

--- tms-backend/src/goal-on-activity/goal-on-activity.module.ts
+++ tms-backend/src/goal-on-activity/goal-on-activity.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GoalOnActivityService } from './goal-on-activity.service';
+import { GoalOnActivityController } from './goal-on-activity.controller';
 
 @Module({
+  controllers: [GoalOnActivityController],
   providers: [GoalOnActivityService],
 })
 export class GoalOnActivityModule {}
--- tms-backend/src/goal-on-activity/goal-on-activity.service.ts
+++ tms-backend/src/goal-on-activity/goal-on-activity.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateGoalOnActivityDto } from './dto/create-goal-on-activity.dto';
+import { UpdateGoalOnActivityDto } from './dto/update-goal-on-activity.dto';
 
 @Injectable()
-export class GoalOnActivityService {}
+export class GoalOnActivityService {
+  create(createGoalOnActivityDto: CreateGoalOnActivityDto) {
+    return 'This action adds a new goalOnActivity';
+  }
+
+  findAll() {
+    return `This action returns all goalOnActivity`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} goalOnActivity`;
+  }
+
+  update(id: number, updateGoalOnActivityDto: UpdateGoalOnActivityDto) {
+    return `This action updates a #${id} goalOnActivity`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} goalOnActivity`;
+  }
+}
