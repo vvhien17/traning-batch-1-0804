b8293f1a24a74c37e2e7f19d0fd3faff01e6ee8f
kienlu <kienlu2000@gmail.com>
2024-08-23 13:59:24 +0700

    feat: add test login case

--- tms-frontend/jest.config.js
+++ tms-frontend/jest.config.js
@@ -1,9 +1,24 @@
 const nextJest = require("next/jest");
+const { compilerOptions } = require("./tsconfig.json");
+
 const createJestConfig = nextJest({
   dir: "./",
 });
+
+const aliasMapper = Object.keys(compilerOptions.paths).reduce((acc, key) => {
+  const alias = `^${key.replace("/*", "/(.*)")}$`;
+  const path = `<rootDir>/${compilerOptions.paths[key][0].replace(
+    "/*",
+    "/$1"
+  )}`;
+  acc[alias] = path;
+  return acc;
+}, {});
+
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: aliasMapper,
 };
+
 module.exports = createJestConfig(customJestConfig);
