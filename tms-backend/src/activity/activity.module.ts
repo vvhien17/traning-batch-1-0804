21d7e5778c5faa3fccd73022c8549ac1b1e6b5ff
hienvv17 <hien.vo@xpodc.com>
2024-08-26 15:35:07 +0700

    [BE] hotfix missing import module

--- tms-backend/src/activity/activity.module.ts
+++ tms-backend/src/activity/activity.module.ts
@@ -4,9 +4,10 @@ import { ActivityController } from './activity.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Activity } from './entities/activity.entity';
 import { User } from '../user/entities/user.entity';
+import { Category } from '../category/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Activity, User])],
+  imports: [TypeOrmModule.forFeature([Activity, User, Category])],
   controllers: [ActivityController],
   providers: [ActivityService],
 })
