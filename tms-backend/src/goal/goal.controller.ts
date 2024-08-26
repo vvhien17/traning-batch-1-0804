e4856b2859be9fd7a01330c6f1cd7b38c32cba67
Tuyen Van <59913038+TuyenVann@users.noreply.github.com>
2024-08-26 12:09:11 +0700

    find all goal by userId

--- tms-backend/src/goal/goal.controller.ts
+++ tms-backend/src/goal/goal.controller.ts
@@ -6,12 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 import { GoalService } from './goal.service';
 import { CreateGoalDto } from './dto/create-goal.dto';
 import { UpdateGoalDto } from './dto/update-goal.dto';
+import { AuthGuard } from '../middleware/auth.guard';
 
 @Controller('goal')
+@UseGuards(AuthGuard)
 export class GoalController {
   constructor(private readonly goalService: GoalService) {}
 
@@ -23,6 +27,11 @@ export class GoalController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.goalService.findOne(+id);
+  }
+  @Get()
+  findAllByUserId(@Request() req) {
+    const userId = req.user.id;
+    return this.goalService.findAllByUserId(userId);
   }
 
   @Patch(':id')
