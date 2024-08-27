54b59d32bd07dc8e0f2ec00f7514e4d6c81821c2
QuanDH <quan.duong@xpodc.com>
2024-08-27 14:57:29 +0700

    fix  api bear and check null category in activity list

--- tms-backend/src/dashboard/dashboard.controller.ts
+++ tms-backend/src/dashboard/dashboard.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { AuthGuard } from '@/middleware/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
@@ -9,6 +11,7 @@ export class DashboardController {
   @Get()
   findAll(@Request() req) {
     const userId = req.user.id;
+    console.log(userId);
     return this.dashboardService.getCategoryTimePercentages(userId);
   }
 }
