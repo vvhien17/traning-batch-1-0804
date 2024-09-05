c169d2c71b22b8d2deb772f5c1310682fa575bb0
hienvv17 <hien.vo@xpodc.com>
2024-09-06 03:29:26 +0700

    failed import

--- tms-backend/src/goal/goal.module.ts
+++ tms-backend/src/goal/goal.module.ts
@@ -4,7 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Goal } from './entities/goal.entity';
 import { User } from '../user/entities/user.entity';
 import { GoalController } from './goal.controller';
-import { GoalOnActivity } from '@/goal-on-activity/entities/goal-on-activity.entity';
+import { GoalOnActivity } from '../goal-on-activity/entities/goal-on-activity.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Goal, User, GoalOnActivity])],
